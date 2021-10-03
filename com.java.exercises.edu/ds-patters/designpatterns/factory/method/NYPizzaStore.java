b80f7a4ca031dcc95e2c26d441ab927657ff57bf
gtzuc <gtzuc@localhost>
2021-10-03 17:43:59 -0500

    Add new patterns

---
+++ com.java.exercises.edu/ds-patters/designpatterns/factory/method/NYPizzaStore.java
@@ -0,0 +1,18 @@
+package designpatterns.factory.method;
+
+public class NYPizzaStore extends PizzaStore {
+
+	Pizza createPizza(String item) {
+		if (item.equals("cheese")) {
+			//return new NYStyleCheesePizza();
+		} else if (item.equals("veggie")) {
+			return new NYStyleVeggiePizza();
+		} else if (item.equals("clam")) {
+			return new NYStyleClamPizza();
+		} else if (item.equals("pepperoni")) {
+		//	return new NYStylePepperoniPizza();
+		} else return null;
+		
+		return null;
+	}
+}
