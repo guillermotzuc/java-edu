aa8e10d800fb4b9ee7a9bd9b27441e8b9a149c48
gtzuc <gtzuc@192.168.1.171>
2021-11-21 10:19:47 -0600

    add rest service

--- spring-boot-kafka/src/main/java/io/confluent/developer/spirngccloud/controllers/RestService.java
+++ spring-boot-kafka/src/main/java/io/confluent/developer/spirngccloud/controllers/RestService.java
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-//@RestController
-//@RequiredArgsConstructor
-//public class RestService {
-//	
-//	private final StreamsBuilderFactoryBean factoryBeam;
-//	
-//	@GetMapping("/count/{word}")
-//	public Long getCount(@PathVariable String word) {
-//		
-//		final KafkaStreams  kafkaStreams = factoryBeam.getKafkaStreams();
-//		final ReadOnlyKeyValueStore<String, Long> counts = 
-//				kafkaStreams.store(StoreQueryParameters.fromNameAndType("counts", QueryableStoreTypes.keyValueStore()));
-//		
-//		return counts.get(word);
-//	}
-//}
+@RestController
+@RequiredArgsConstructor
+public class RestService {
+	
+	private final StreamsBuilderFactoryBean factoryBeam;
+	
+	@GetMapping("/count/{word}")
+	public Long getCount(@PathVariable String word) {
+		
+		final KafkaStreams  kafkaStreams = factoryBeam.getKafkaStreams();
+		final ReadOnlyKeyValueStore<String, Long> counts = 
+				kafkaStreams.store(StoreQueryParameters.fromNameAndType("counts", QueryableStoreTypes.keyValueStore()));
+		
+		return counts.get(word);
+	}
+}
